3862b386324e2070850c62eb40778ba8accc6861
abhinav <abhinavkarn55@gmail.com>
2017-10-23 00:43:58 +0530

    service discovery done

--- employee-consumer/src/main/java/com/abhinav/employee_consumer/controller/ConsumerControllerClient.java
+++ employee-consumer/src/main/java/com/abhinav/employee_consumer/controller/ConsumerControllerClient.java
@@ -1,19 +1,37 @@
 package com.abhinav.employee_consumer.controller;
 
 import java.io.IOException;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
+@Controller
 public class ConsumerControllerClient {
 	
+	@Autowired
+	private DiscoveryClient discoveryClient;
+	
 	public void getEmployee() throws RestClientException, IOException {
+		
+		List<ServiceInstance> instances=discoveryClient.getInstances("employee-producer");
+		ServiceInstance serviceInstance=instances.get(0);
+		
+		String baseUrl=serviceInstance.getUri().toString();
+		
+		
 
-		String baseUrl = "http://localhost:8080/emp";
+		baseUrl=baseUrl+"/emp";
 		RestTemplate restTemplate = new RestTemplate();
 		ResponseEntity<String> response=null;
 		try{
